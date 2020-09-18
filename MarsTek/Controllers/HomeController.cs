3b56313cb2a46e1a4e16f7bb9d4b7f8993c272c3
Ian Marrs <ian.t.marrs@gmail.com>
2020-09-18 16:36:28 -0600

    Added contact email functionality, successfully

--- MarsTek/Controllers/HomeController.cs
+++ MarsTek/Controllers/HomeController.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MarsTek.Models;
+using MarsTek.Dtos;
+using System.Net.Mail;
+using System.Net;
 
 namespace MarsTek.Controllers
 {
@@ -25,6 +28,49 @@ namespace MarsTek.Controllers
 
         public IActionResult Privacy()
         {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ContactUs(SendMailDto sendMail)
+        {
+            if (!ModelState.IsValid) return View();
+
+            try
+            {
+                MailMessage mail = new MailMessage();
+                mail.From = new MailAddress("marrscoder@gmail.com");
+                mail.To.Add("marrscoder@gmail.com");
+
+                mail.Subject = sendMail.Subject;
+
+                mail.IsBodyHtml = true;
+
+                string content = "Name : " + sendMail.Name;
+                content += "<br /> Message :" + sendMail.Message;
+
+                mail.Body = content;
+
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+
+                NetworkCredential networkCredential = new NetworkCredential("marrscoder@gmail.com", "Skiing0709!");
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = networkCredential;
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+                smtp.Send(mail);
+
+                ViewBag.Message = "Message sent!";
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Message = ex.Message.ToString();
+            }
             return View();
         }
 
